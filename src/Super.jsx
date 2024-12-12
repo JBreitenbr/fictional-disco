f47d949be556b6ee1bcccd41e0cac083d036c6e0
Julia Breitenbruch <119942939+JBreitenbr@users.noreply.github.com>
2024-12-12 23:47:56 +0000

    gg

--- src/Super.jsx
+++ src/Super.jsx
@@ -1,7 +1,7 @@
 import Test from "./Test";
 const Super = () => {
   return (
-    <div>
+    <div style={{display:"flex",flexDirection:"row"}}>
       <Test cl="b1" range="short-term"/>
       <Test cl="b2" range="medium-term"/>
     </div>
