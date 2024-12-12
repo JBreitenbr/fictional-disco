e17986c4add0f1b887ffbe9237b8aa6e77f59b9b
Julia Breitenbruch <119942939+JBreitenbr@users.noreply.github.com>
2024-12-12 17:05:28 +0000

    g

--- src/Test.jsx
+++ src/Test.jsx
@@ -1,5 +1,5 @@
-import SpotifyAuthButton from "./SpotifyAuthButton";import * as SpotifyFunctions from './spotiFunctions.js'
-import {useState,useEffect} from "react";
+import SpotifyAuthButton from "./SpotifyAuthButton";
+import {useState} from "react";
 const Test = () => {
   const { VITE_CLIENT_ID } = import.meta.env;
 const { VITE_REDIRECT_URI } = import.meta.env;
@@ -13,26 +13,6 @@ const scopes = [
       setAccessToken(token);
       console.log("Received Access Token:", token);
     };
-  const [favArtists, setFavArtists] = useState([]);
-useEffect(() => {
-    (async function() {
-        await SpotifyFunctions.setAccessToken(accessToken);
-        const artists = await SpotifyFunctions.getFavArtists();
-//document.write(artists.items[0].images[2].url);
-//document.write(artists.items.length);
-let length=artists.items.length;
-let artArr=[];
-for(let i=0;i<length;i++){
-  artArr.push({});
-  artArr[i]["name"]=artists.items[i].name;
-  artArr[i]["id"]=artists.items[i].id;
-  artArr[i]["image"]=artists.items[i].images[2].url;
-}
-      setFavArtists(artArr);
-
-    })();
-  }, [accessToken])
-
 return (
       <div>
         <h1>Spotify Authentication Example</h1>
@@ -44,7 +24,9 @@ return (
             onAccessTokenReceived={handleAccessToken}
           />
         ) : (
-          <ol>{favArtists.map((item)=><li key={item.id}><h2>{item.name}</h2><img style={{width:"100px",height:"100px"}} src={item.image}/></li>)}</ol>
-)}</div>)
+          <p>Access Token: {accessToken}</p>
+        )}
+      </div>
+    );
 }
 export default Test;
