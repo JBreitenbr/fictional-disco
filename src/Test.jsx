5f2d817d0e70dc7fcdbcfb487c24f08753c14661
Julia Breitenbruch <119942939+JBreitenbr@users.noreply.github.com>
2024-12-15 10:27:02 +0000

    orig

--- src/Test.jsx
+++ src/Test.jsx
@@ -24,19 +24,23 @@ useEffect(() => {
   let artists1 = await SpotifyFunctions.getFavArtists1();
   let artists2 = await SpotifyFunctions.getFavArtists2();
   let artists3 = await SpotifyFunctions.getFavArtists3()
-let length=artists1.items.length;
+let length1=artists1.items.length;
+let length2=artists2.items.length;
+let length3=artists3.items.length;
 let artArr1=[];
 let artArr2=[];
 let artArr3=[];
-for(let i=0;i<length;i++){
+for(let i=0;i<length1;i++){
   artArr1.push({});
   artArr1[i]["name"]=artists1.items[i].name;
   artArr1[i]["id"]=artists1.items[i].id;
-  artArr1[i]["image"]=artists1.items[i].images[2].url;
+  artArr1[i]["image"]=artists1.items[i].images[2].url;}
+for(let i=0;i<length2;i++){
   artArr2.push({});
   artArr2[i]["name"]=artists2.items[i].name;
-  artAri2[i]["id"]=artists2.items[i].id;
-  artArr2[i]["image"]=artists2.items[i].images[2].url;
+  artArr2[i]["id"]=artists2.items[i].id;
+  artArr2[i]["image"]=artists2.items[i].images[2].url;}
+for(let i=0;i<length3;i++){
   artArr3.push({});
   artArr3[i]["name"]=artists3.items[i].name;
   artArr3[i]["id"]=artists3.items[i].id;
