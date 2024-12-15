20f578406c4c6828c477769feec0ff8a830f4a35
Julia Breitenbruch <119942939+JBreitenbr@users.noreply.github.com>
2024-12-15 15:45:43 +0000

    j

--- src/Super.jsx
+++ src/Super.jsx
@@ -2,7 +2,7 @@ import {useState} from "react"
 import Test from "./Test";
 const Super = () => {
   const [taekscht,setTaekscht] = useState("Login with Spotify");
-  const [zahl,setZahl]= usetState(1);
+  const [zahl,setZahl]= useState(1);
   return (
     <div style={{display:"flex",flexDirection:"row"}}><button onClick={()=>setZahl(1)}>last four weeks</button>
 <button onClick={()=>setZahl(2)}>last six months</button>
