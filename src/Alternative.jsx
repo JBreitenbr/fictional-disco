b2c8a6c12ea3f143094d5fafe15ca2df80a522df
Julia Breitenbruch <119942939+JBreitenbr@users.noreply.github.com>
2024-12-15 13:04:28 +0000

    j

--- src/Alternative.jsx
+++ src/Alternative.jsx
@@ -40,7 +40,7 @@ const scopes = [
           const accessToken = params.get("access_token");
           if (accessToken) {
             setAccessToken(accessToken);
-
+/*
 SpotifyFunctions.setAccessToken(accessToken);  
             let artists=  SpotifyFunctions.getFavArtists();
     let length=artists.items.length;
@@ -51,7 +51,7 @@ for(let i=0;i<length;i++){
   artArr[i]["id"]=artists.items[i].id;
   artArr[i]["image"]=artists.items[i].images[2].url;
 }
-    setFavArtists(artArr);       }//onAccessTokenReceived(accessToken); // Pass the token to the parent component
+    setFavArtists(artArr);  */     }//onAccessTokenReceived(accessToken); // Pass the token to the parent component
             popup.close();
             clearInterval(timer);
           }
@@ -62,7 +62,7 @@ for(let i=0;i<length;i++){
     }, 500);
   };
 
-  return <div style={{backgroundColor:"lightblue"}}><button onClick={handleLogin} >Login with Spotify</button><div> {accessToken}</div><div style={{backgroundColor:"lightblue"}}><ol style={{width:"100vw"}}>{favArtists.map((item)=><li key={item.id}><h2>{item.name}</h2><img style={{width:"100px",height:"100px"}} src={item.image}/></li>)}</ol></div></div>;
+  return <div style={{backgroundColor:"purple"}}><button onClick={handleLogin} >Login with Spotify</button><div> {accessToken}</div><div style={{backgroundColor:"lightblue"}}><ol style={{width:"100vw"}}>{favArtists.map((item)=><li key={item.id}><h2>{item.name}</h2><img style={{width:"100px",height:"100px"}} src={item.image}/></li>)}</ol></div></div>;
 };
 
 export default Alternative;
